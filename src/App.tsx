c341a9030aa9e702e530bbbfedcbd3107ed0e67e
b <btn0s@users.noreply.github.com>
2025-12-31 13:12:32 -0700

    Add Vercel Analytics tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/react";
 import { Canvas } from "./components/Canvas";
 
 export function App() {
   return (
     <div className="dark h-full w-full overflow-hidden">
       <Canvas />
+      <Analytics />
     </div>
   );
 }
